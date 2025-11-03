788b428882dbacb7d3395dbebb006bbf6d9aabc0
erdincdagli35 <3rd1fbslash@gmail.com>
2025-11-03 20:07:12 +0300

    minor

--- src/main/java/com/edsoft/order_service/controller/OrderController.java
+++ src/main/java/com/edsoft/order_service/controller/OrderController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin
+@CrossOrigin("https://beachorder.up.railway.app/")
 public class OrderController {
 
     @Autowired
